a35de4fe806af41bdbd0908ce8efad3c9127b635
Matthew Corritore <mcorrito@cedar1.cedar.computecanada.ca>
2020-04-27 16:21:21 -0700

    regular update

--- README.md
+++ README.md
@@ -3,15 +3,23 @@ This file details the structure of the Glassdoor sql database.
 Cleaning notes: there are a few reviewId==-1 that need to be removed, as well as some duplicates reviewIds
 
 ..* reviewProCon
-reviewId, numeric
-pros, varchar
-cons, varchar
+reviewId numeric,
+pros TEXT,
+cons TEXT
 
 ..* reviewFeedback
-reviewId, numeric
-feedback, varchar
+reviewId numeric,
+feedback TEXT
 
 ..* reviewRatings
-reviewId, numeric
-
+reviewId numeric,
+OverallRating numeric,
+CareerOpps numeric,
+CompensationBenefits numeric,
+SeniorLeadership numeric,
+Worklife numeric,
+CultureValues numeric,
+RecommendFriend varchar(3),
+BusinessOutlook 27,
+CEO 28,
 
--- preprocess_csv.py
+++ preprocess_csv.py
@@ -5,24 +5,96 @@ csv.field_size_limit(sys.maxsize)
 
 def extract():
 
+    header = ['reviewID',
+              'employerID',
+              'userID',
+              'gender',
+              'birthYear',
+              'highestEducation',
+              'jobTitleID',
+              'JobTitle',
+              'GOC',
+              'GOCconfidence',
+              'MGOC',
+              'MGOCconfidence',
+              'reviewDateTime',
+              'isCurrentJobFlag',
+              'jobEndingYear',
+              'OverallRating',
+              'CareerOpps',
+              'CompensationBenefits',
+              'SeniorLeadership',
+              'Worklife',
+              'CultureValues',
+              'CEO',
+              'employerName',
+              'employerTypeCode',
+              'numberEmployees',
+              'annualRevenue',
+              'industry',
+              'sector',
+              'pros',
+              'cons',
+              'feedback']
+    
     masterFile = open("/project/def-mcorrito/mcorrito/reviews_external_stanford.csv",'rU')
     newFile = open("/project/def-mcorrito/mcorrito/gd_preprocess.csv",'w')
     write = csv.writer(newFile)
-
+    write.writerow(header)
+    
     filtered = (line.replace('\0',' ') for line in masterFile)
     filtered = (line.replace('\r\n','. ') for line in filtered)
     filtered = (line.replace('\n','. ') for line in filtered)
     read = csv.reader(filtered,delimiter=",")
 
-    header = next(read)
-    write.writerow(header)
+    next(read)
     
     for row in read:
         if len(row)==39:
 
-            write.writerow(row)
+            dataRow = [row[0],
+                       row[1],
+                       row[2],
+                       row[3],
+                       row[4],
+                       row[5],
+                       row[11],
+                       row[12],
+                       row[13],
+                       row[14],
+                       row[15],
+                       row[16],
+                       row[17],
+                       row[18],
+                       row[19],
+                       row[20],
+                       row[21],
+                       row[22],
+                       row[23],
+                       row[24],
+                       row[25],
+                       row[28],
+                       row[29],
+                       row[31],
+                       row[32],
+                       row[33],
+                       row[34],
+                       row[35],
+                       row[36],
+                       row[37],
+                       row[38]]
+            
+            write.writerow(dataRow)
 
     masterFile.close()
     newFile.close()
 
 extract()
+
+
+
+
+
+
+
+
