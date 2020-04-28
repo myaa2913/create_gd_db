d9512751517d576a4090cb1ec3af3e034b617ff6
Matthew Corritore <mcorrito@cedar5.cedar.computecanada.ca>
2020-04-28 15:21:58 -0700

    regular update

--- create_db.sql
+++ create_db.sql
@@ -1,38 +1,38 @@
 -- DROP TABLE raw;
 
-CREATE TABLE raw
-(
-    reviewID numeric, 
-    employerID numeric,
-    userID numeric,
-    gender TEXT,
-    birthYear numeric,
-    highestEducation TEXT,
-    jobTitleID numeric,
-    JobTitle TEXT,
-    GOC TEXT,
-    GOCconfidence numeric,
-    MGOC TEXT,
-    MGOCconfidence numeric,
-    reviewDateTime timestamp,
-    isCurrentJobFlag smallint,
-    jobEndingYear smallint,
-    OverallRating numeric,
-    CareerOpps numeric,
-    CompensationBenefits numeric,
-    SeniorLeadership numeric,
-    Worklife numeric,
-    CultureValues numeric,
-    employerName TEXT,
-    employerTypeCode varchar,
-    numberEmployees numeric,
-    annualRevenue numeric,
-    industry TEXT,
-    sector TEXT,
-    pros TEXT,
-    cons TEXT
-);
+-- CREATE TABLE raw
+-- (
+--     reviewID numeric, 
+--     employerID numeric,
+--     userID numeric,
+--     gender TEXT,
+--     birthYear numeric,
+--     highestEducation TEXT,
+--     jobTitleID numeric,
+--     JobTitle TEXT,
+--     GOC TEXT,
+--     GOCconfidence numeric,
+--     MGOC TEXT,
+--     MGOCconfidence numeric,
+--     reviewDateTime timestamp,
+--     isCurrentJobFlag smallint,
+--     jobEndingYear smallint,
+--     OverallRating numeric,
+--     CareerOpps numeric,
+--     CompensationBenefits numeric,
+--     SeniorLeadership numeric,
+--     Worklife numeric,
+--     CultureValues numeric,
+--     employerName TEXT,
+--     employerTypeCode varchar,
+--     numberEmployees numeric,
+--     annualRevenue numeric,
+--     industry TEXT,
+--     sector TEXT,
+--     pros TEXT,
+--     cons TEXT
+-- );
 
-\copy raw FROM '~/projects/def-mcorrito/mcorrito/gd_preprocess.csv' DELIMITER ',' quote '"' CSV HEADER;
+-- \copy raw FROM '~/projects/def-mcorrito/mcorrito/gd_preprocess.csv' DELIMITER ',' quote '"' CSV HEADER;
 
 
