10414f7447a92e8f954795e579d2496fbb46d273
Matthew Corritore <mcorrito@cedar5.cedar.computecanada.ca>
2020-04-30 15:08:44 -0700

    added more culture measures to sql

--- add_cult_measures.sql
+++ add_cult_measures.sql
@@ -1,24 +1,24 @@
 --NOTE: add 2016 annual JS/interpersonal diversity measure
 
 --ALTER TABLE firm_chars
---ADD COLUMN js_500_f_annual_0 numeric,
---ADD COLUMN num_reviews_0 numeric;
+--ADD COLUMN js_500_f_annual_old numeric,
+--ADD COLUMN nr_500_f_annual_old numeric;
 
 -- CREATE TABLE cult_measure (
 --     employerid numeric,
 --     year numeric,
---     js_500_f_annual_0 numeric,
---     num_reviews_0 numeric
+--     js_500_f_annual_old numeric,
+--     nr_500_f_annual_old numeric
 -- );
 
 --\copy cult_measure FROM '~/projects/def-mcorrito/mcorrito/create_gd_db/cult_measure_upload/JS_measures_combined_annual.csv' DELIMITER ',' quote '"' CSV HEADER;
 
--- INSERT INTO firm_chars(employerid,year,js_500_f_annual_0, num_reviews_0) 
--- SELECT employerid, year, js_500_f_annual_0, num_reviews_0
+-- INSERT INTO firm_chars(employerid,year,js_500_f_annual_old, nr_500_f_annual_old) 
+-- SELECT employerid, year, js_500_f_annual_old, nr_500_f_annual_old
 -- FROM cult_measure 
 -- ON CONFLICT (employerid,year)
 -- DO UPDATE
--- SET js_500_f_annual_0 = EXCLUDED.js_500_f_annual_0, num_reviews_0 = EXCLUDED.num_reviews_0; 
+-- SET js_500_f_annual_old = EXCLUDED.js_500_f_annual_old, nr_500_f_annual_old = EXCLUDED.nr_500_f_annual_old; 
 
 --DROP TABLE cult_measure;
 
@@ -26,25 +26,116 @@
 
 --NOTE: add 2016 annual herf/intrapersonal diversity measure
 --ALTER TABLE firm_chars
---ADD COLUMN h_500_f_ind_annual_0 numeric;
+--ADD COLUMN h_500_f_ind_annual_old numeric;
 
 -- CREATE TABLE cult_measure (
 --     employerid numeric,
 --     year numeric,
---     h_500_f_ind_annual_0 numeric
+--     h_500_f_ind_annual_old numeric
 -- );
 
 -- \copy cult_measure FROM '~/projects/def-mcorrito/mcorrito/create_gd_db/cult_measure_upload/h_measures_combined_ind_annual.csv' DELIMITER ',' quote '"' CSV HEADER;
 
--- INSERT INTO firm_chars(employerid,year,h_500_f_ind_annual_0) 
--- SELECT employerid, year, h_500_f_ind_annual_0
+-- INSERT INTO firm_chars(employerid,year,h_500_f_ind_annual_old) 
+-- SELECT employerid, year, h_500_f_ind_annual_old
 -- FROM cult_measure 
 -- ON CONFLICT (employerid,year)
 -- DO UPDATE
--- SET h_500_f_ind_annual_0 = EXCLUDED.h_500_f_ind_annual_0; 
+-- SET h_500_f_ind_annual_old = EXCLUDED.h_500_f_ind_annual_old; 
 
 -- DROP TABLE cult_measure;
 
 
 
---NOTE: add 2018 annual JS/interpersonal diversity measure
+-- --NOTE: add 2018 annual JS/interpersonal diversity measure
+-- ALTER TABLE firm_chars
+-- ADD COLUMN js_500_f_annual_new numeric,
+-- ADD COLUMN nr_500_f_annual_new numeric;
+
+-- CREATE TABLE cult_measure (
+--     employerid numeric,
+--     year numeric,
+--     js_500_f_annual_new numeric,
+--     nr_500_f_annual_new numeric
+-- );
+
+-- \copy cult_measure FROM '~/projects/def-mcorrito/mcorrito/create_gd_db/cult_measure_upload/JS_measures_combined_annual_new.csv' DELIMITER ',' quote '"' CSV HEADER;
+
+-- INSERT INTO firm_chars(employerid,year,js_500_f_annual_new, nr_500_f_annual_new) 
+-- SELECT employerid, year, js_500_f_annual_new, nr_500_f_annual_new
+-- FROM cult_measure 
+-- ON CONFLICT (employerid,year)
+-- DO UPDATE
+-- SET js_500_f_annual_new = EXCLUDED.js_500_f_annual_new, nr_500_f_annual_new = EXCLUDED.nr_500_f_annual_new; 
+
+-- DROP TABLE cult_measure;
+
+
+
+--NOTE: add 2018 annual herf/intrapersonal diversity measure
+-- ALTER TABLE firm_chars
+-- ADD COLUMN h_500_f_ind_annual_new numeric;
+
+-- CREATE TABLE cult_measure (
+--     employerid numeric,
+--     year numeric,
+--     h_500_f_ind_annual_new numeric
+-- );
+
+-- \copy cult_measure FROM '~/projects/def-mcorrito/mcorrito/create_gd_db/cult_measure_upload/h_measures_combined_ind_annual_new.csv' DELIMITER ',' quote '"' CSV HEADER;
+
+-- INSERT INTO firm_chars(employerid,year,h_500_f_ind_annual_new) 
+-- SELECT employerid, year, h_500_f_ind_annual_new
+-- FROM cult_measure 
+-- ON CONFLICT (employerid,year)
+-- DO UPDATE
+-- SET h_500_f_ind_annual_new = EXCLUDED.h_500_f_ind_annual_new; 
+
+-- DROP TABLE cult_measure;
+
+
+
+--NOTE: add 2018 annual within-firm JS/interpersonal diversity measure
+-- ALTER TABLE firm_chars
+-- ADD COLUMN js_within_40_f_annual_new numeric,
+-- ADD COLUMN nr_js_within_40_f_annual_new numeric;
+
+-- CREATE TABLE cult_measure (
+--     employerid numeric,
+--     year numeric,
+--     js_within_40_f_annual_new numeric,
+--     nr_js_within_40_f_annual_new numeric
+-- );
+
+-- \copy cult_measure FROM '~/projects/def-mcorrito/mcorrito/create_gd_db/cult_measure_upload/JS_measures_within_40_annual.csv' DELIMITER ',' quote '"' CSV HEADER;
+
+-- INSERT INTO firm_chars(employerid,year,js_within_40_f_annual_new, nr_js_within_40_f_annual_new) 
+-- SELECT employerid, year, js_within_40_f_annual_new, nr_js_within_40_f_annual_new
+-- FROM cult_measure 
+-- ON CONFLICT (employerid,year)
+-- DO UPDATE
+-- SET js_within_40_f_annual_new = EXCLUDED.js_within_40_f_annual_new, nr_js_within_40_f_annual_new = EXCLUDED.nr_js_within_40_f_annual_new; 
+
+-- DROP TABLE cult_measure;
+
+
+--NOTE: add 2018 annual within-firm herf/intrapersonal diversity measure
+-- ALTER TABLE firm_chars
+-- ADD COLUMN h_within_ind_40_f_annual_new numeric;
+
+-- CREATE TABLE cult_measure (
+--     employerid numeric,
+--     year numeric,
+--     h_within_ind_40_f_annual_new numeric
+-- );
+
+-- \copy cult_measure FROM '~/projects/def-mcorrito/mcorrito/create_gd_db/cult_measure_upload/h_measures_within_40_annual.csv' DELIMITER ',' quote '"' CSV HEADER;
+
+-- INSERT INTO firm_chars(employerid,year,h_within_ind_40_f_annual_new) 
+-- SELECT employerid, year, h_within_ind_40_f_annual_new
+-- FROM cult_measure 
+-- ON CONFLICT (employerid,year)
+-- DO UPDATE
+-- SET h_within_ind_40_f_annual_new = EXCLUDED.h_within_ind_40_f_annual_new; 
+
+-- DROP TABLE cult_measure;
